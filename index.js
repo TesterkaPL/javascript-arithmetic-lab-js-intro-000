4675c6ddf9a8221593b5ee68a5d17ea6356aa465
ambitious-waterfall-9648 <porebadorota2@gmail.com>
2019-03-04 17:51:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function dec(n){
 function makeInt(n, base){
   var parsed = parseInt(n, 10);
   if (isNaN(parsed)) {return 0;}
-  return parsed
+  return parsed;
 }
 
 function preserveDecimal(n){
