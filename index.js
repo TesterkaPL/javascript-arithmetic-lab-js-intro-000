547f487ac44269eaecc1c43016e9b181d6958f31
ambitious-waterfall-9648 <porebadorota2@gmail.com>
2019-03-04 17:30:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b){
+  return a + b
+}
+add(50,80)
+
+function subtract(a, b){
+  return a - b
+}
+subtract(600,130)
+
+function multiply(a, b){
+  return a * b
+}
+multiply(20,30)
+
+function divide(a, b){
+  return a / b
+}
+divide(600,3)
+
+function inc(n){
+  return ++n
+}
+
+function dec(n){
+  return --n
+}
+
+function makeInt(n){
+  var parsed = parseInt(n);
+  if (isNaN(parsed)) {
+    return 0
+  }
+  return parseInt('n', 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat('2.222')
+}
